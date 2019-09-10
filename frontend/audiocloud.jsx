faf807bb6d42fe515789ad7e470073bd0d362513
Ryan Sullivan <mrsulliv@gmail.com>
2019-09-10 16:07:45 -0700

    Remove logger for heroku

--- frontend/audiocloud.jsx
+++ frontend/audiocloud.jsx
@@ -20,6 +20,4 @@ document.addEventListener("DOMContentLoaded", () => {
   
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
-
-  window.store = store;//Remove in the furtire
 });
