2f907d05009f18c75b6fd12dbd7cb227a0d13e5b
Ryan Sullivan <mrsulliv@gmail.com>
2019-10-30 13:18:52 -0700

    Implement mouse drag for seek bar

--- frontend/components/playcontrols/progress_bar.jsx
+++ frontend/components/playcontrols/progress_bar.jsx
@@ -10,16 +10,34 @@ function ProgressBar({curTime, max}) {
         audio.currentTime = percent * max;
     };
 
+    function drag(e) {
+        const bar = document.getElementById('bar-progress');
+        const start = bar.getBoundingClientRect().x;
+        const pos = e.clientX - start;
+        const percent = pos / bar.getBoundingClientRect().width;
+        const audio = document.getElementById('audio');
+        audio.currentTime = percent * max;
+    }
+
+    function handleDrag(e) {
+        document.addEventListener('mousemove', drag);
+        document.addEventListener('mouseup', () => {
+            console.log('stop drag');
+            document.removeEventListener('mousemove', drag);
+        });
+    }
+
     return (
         <div
             id="bar-progress"
             className="bar-progress"
             onClick={handleClick}
             style={{
-                background: `linear-gradient(to right, orange ${(curTime / max) * 100}%, white 0`
+                background: `linear-gradient(to right, #f50 ${(curTime / max) * 100}%, white 0`
             }}
         >
             <span
+                onMouseDown={handleDrag}
                 className="bar-progress-knob"
                 style={{ left: `${(curTime / max) * 100 - 2}%` }}
             />
